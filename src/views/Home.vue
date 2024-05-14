ec2abdf16236fbe2580757053b624f56a4499dd9
Robin <m.riabenko@ejaw.net>
2024-05-14 09:20:54 +0300

    [feature]: added a registration page and a simple form

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home-page',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
